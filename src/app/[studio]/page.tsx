f31acaed4b20c4f1add16647f0baae5f801c7a0f
Atharv Deshmukh <79937868+ADthekid@users.noreply.github.com>
2024-02-03 14:03:29 -0800

    Styled Studio Page

--- src/app/[studio]/page.tsx
+++ src/app/[studio]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import Piano from "@/components/instruments/Piano";
 import { useState, useEffect } from "react";
+import { FaCircle, FaPlay } from "react-icons/fa6";
 
 export default function Page({ params }: { params: { studio: string } }) {
 	const { studio } = params;
@@ -16,10 +18,10 @@ export default function Page({ params }: { params: { studio: string } }) {
 
 	const [selectedOption, setSelectedOption] = useState("loops");
 	return (
-		<div className="grid grid-cols-3 gap-4">
+		<div className="grid grid-cols-3">
 			{/* Left column */}
-			<div className="col-span-1 bg-gray-200 h-screen">
-				<div className="p-4 bg-white border-b border-gray-200">
+			<div className="col-span-1 bg-blue text-black h-screen border-r-8 border-black">
+				<div className="p-4  border-b-8 border-black">
 					<div className="flex items-center justify-between">
 						<h3 className="text-lg font-medium">Options</h3>
 					</div>
@@ -44,21 +46,40 @@ export default function Page({ params }: { params: { studio: string } }) {
 				</div>
 				{/* Content for the selected option will go here */}
 				{selectedOption === "loops" && (
-					<div className="p-4 bg-white">
+					<div className="p-4">
 						<h3 className="text-lg font-medium">Loops content</h3>
 						{/* Loops content goes here */}
 					</div>
 				)}
 				{selectedOption === "sounds" && (
-					<div className="p-4 bg-white">
+					<div className="p-4">
 						<h3 className="text-lg font-medium">Sounds content</h3>
-						{/* Sounds content goes here */}
+						<Piano />
 					</div>
 				)}
 			</div>
 
 			{/* Right column (two-thirds of the screen) */}
-			<div className="col-span-2">{/* Main content will go here */}</div>
+			<main className="col-span-2">
+				{/* Main content will go here */}
+
+				<div className="flex justify-between border-black border-b-8 py-6 px-4">
+					<div>
+						<span className="leading-7 uppercase font-black">Studio Code</span>
+						<br />
+						<span className="leading-8 text-5xl font-black">{studio}</span>
+					</div>
+
+					<div className="flex space-x-2">
+						<button className="bg-yellow border-8 border-black grid place-items-center h-18 w-16 rounded-xl text-3xl font-black">
+							<FaCircle color="#ff6767" />
+						</button>
+						<button className="bg-yellow border-8 border-black grid place-items-center h-18 w-16 rounded-xl text-3xl font-black">
+							<FaPlay color="#317b00" />
+						</button>
+					</div>
+				</div>
+			</main>
 		</div>
 	);
 }
