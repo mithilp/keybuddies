1187df62ddc10f009f569ef8254fe92bed64c46a
mithilp <ping.mithil@gmail.com>
2024-02-06 16:46:40 -0800

    react-piano fixed

--- src/components/instruments/Piano.tsx
+++ src/components/instruments/Piano.tsx
@@ -7,6 +7,7 @@ import {
 	Piano as ReactPiano,
 	KeyboardShortcuts,
 	MidiNumbers,
+	//@ts-ignore
 } from "react-piano";
 import "react-piano/dist/styles.css";
 
@@ -17,6 +18,8 @@ const Piano = () => {
 
 	const [firstNote, setFirstNote] = useState(MidiNumbers.fromNote("c3"));
 	const [lastNote, setLastNote] = useState(MidiNumbers.fromNote("f5"));
+
+	const [time, setTime] = useState(0);
 
 	const keyboardShortcuts = KeyboardShortcuts.create({
 		firstNote: firstNote,
@@ -34,6 +37,9 @@ const Piano = () => {
 			setLoaded(true);
 		});
 	}, []);
+
+	const [sequence, setSequence] = useState([]);
+	const [recording, setRecording] = useState(false);
 
 	return (
 		<>
@@ -59,177 +65,169 @@ const Piano = () => {
 
 						{loaded ? (
 							<div className="space-y-4">
-								<div className="flex items-end justify-between">
-									<div className="flex space-x-2">
-										<div>
-											<label className="text-xl" htmlFor="firstNote">
-												First Note:
-											</label>
-											<select
-												className="w-full bg-yellow border-8 border-black p-2 rounded-xl"
-												value={firstNote.toString()}
-												onChange={(e) => {
-													setFirstNote(Number(e.target.value));
-												}}
-											>
-												<option value="12">C0</option>
-												<option value="14">D0</option>
-												<option value="16">E0</option>
-												<option value="17">F0</option>
-												<option value="19">G0</option>
-												<option value="21">A0</option>
-												<option value="23">B0</option>
-												<option value="24">C1</option>
-												<option value="26">D1</option>
-												<option value="28">E1</option>
-												<option value="29">F1</option>
-												<option value="31">G1</option>
-												<option value="33">A1</option>
-												<option value="35">B1</option>
-												<option value="36">C2</option>
-												<option value="38">D2</option>
-												<option value="40">E2</option>
-												<option value="41">F2</option>
-												<option value="43">G2</option>
-												<option value="45">A2</option>
-												<option value="47">B2</option>
-												<option value="48">C3</option>
-												<option value="50">D3</option>
-												<option value="52">E3</option>
-												<option value="53">F3</option>
-												<option value="55">G3</option>
-												<option value="57">A3</option>
-												<option value="59">B3</option>
-												<option value="60">C4</option>
-												<option value="62">D4</option>
-												<option value="64">E4</option>
-												<option value="65">F4</option>
-												<option value="67">G4</option>
-												<option value="69">A4</option>
-												<option value="71">B4</option>
-												<option value="72">C5</option>
-												<option value="74">D5</option>
-												<option value="76">E5</option>
-												<option value="77">F5</option>
-												<option value="79">G5</option>
-												<option value="81">A5</option>
-												<option value="83">B5</option>
-												<option value="84">C6</option>
-												<option value="86">D6</option>
-												<option value="88">E6</option>
-												<option value="89">F6</option>
-												<option value="91">G6</option>
-												<option value="93">A6</option>
-												<option value="95">B6</option>
-												<option value="96">C7</option>
-												<option value="98">D7</option>
-												<option value="100">E7</option>
-												<option value="101">F7</option>
-												<option value="103">G7</option>
-												<option value="105">A7</option>
-												<option value="107">B7</option>
-												<option value="108">C8</option>
-												<option value="110">D8</option>
-												<option value="112">E8</option>
-												<option value="113">F8</option>
-												<option value="115">G8</option>
-												<option value="117">A8</option>
-												<option value="119">B8</option>
-												<option value="120">C9</option>
-												<option value="122">D9</option>
-												<option value="124">E9</option>
-												<option value="125">F9</option>
-												<option value="127">G9</option>
-											</select>
-										</div>
-
-										<div>
-											<label className="text-xl" htmlFor="firstNote">
-												Last Note:
-											</label>
-											<select
-												className="w-full bg-yellow border-8 border-black p-2 rounded-xl"
-												value={lastNote.toString()}
-												onChange={(e) => {
-													setLastNote(Number(e.target.value));
-												}}
-											>
-												<option value="12">C0</option>
-												<option value="14">D0</option>
-												<option value="16">E0</option>
-												<option value="17">F0</option>
-												<option value="19">G0</option>
-												<option value="21">A0</option>
-												<option value="23">B0</option>
-												<option value="24">C1</option>
-												<option value="26">D1</option>
-												<option value="28">E1</option>
-												<option value="29">F1</option>
-												<option value="31">G1</option>
-												<option value="33">A1</option>
-												<option value="35">B1</option>
-												<option value="36">C2</option>
-												<option value="38">D2</option>
-												<option value="40">E2</option>
-												<option value="41">F2</option>
-												<option value="43">G2</option>
-												<option value="45">A2</option>
-												<option value="47">B2</option>
-												<option value="48">C3</option>
-												<option value="50">D3</option>
-												<option value="52">E3</option>
-												<option value="53">F3</option>
-												<option value="55">G3</option>
-												<option value="57">A3</option>
-												<option value="59">B3</option>
-												<option value="60">C4</option>
-												<option value="62">D4</option>
-												<option value="64">E4</option>
-												<option value="65">F4</option>
-												<option value="67">G4</option>
-												<option value="69">A4</option>
-												<option value="71">B4</option>
-												<option value="72">C5</option>
-												<option value="74">D5</option>
-												<option value="76">E5</option>
-												<option value="77">F5</option>
-												<option value="79">G5</option>
-												<option value="81">A5</option>
-												<option value="83">B5</option>
-												<option value="84">C6</option>
-												<option value="86">D6</option>
-												<option value="88">E6</option>
-												<option value="89">F6</option>
-												<option value="91">G6</option>
-												<option value="93">A6</option>
-												<option value="95">B6</option>
-												<option value="96">C7</option>
-												<option value="98">D7</option>
-												<option value="100">E7</option>
-												<option value="101">F7</option>
-												<option value="103">G7</option>
-												<option value="105">A7</option>
-												<option value="107">B7</option>
-												<option value="108">C8</option>
-												<option value="110">D8</option>
-												<option value="112">E8</option>
-												<option value="113">F8</option>
-												<option value="115">G8</option>
-												<option value="117">A8</option>
-												<option value="119">B8</option>
-												<option value="120">C9</option>
-												<option value="122">D9</option>
-												<option value="124">E9</option>
-												<option value="125">F9</option>
-												<option value="127">G9</option>
-											</select>
-										</div>
+								<div className="flex space-x-4">
+									<div className="flex items-center space-x-2">
+										<label className="text-xl" htmlFor="firstNote">
+											First Note:
+										</label>
+										<select
+											className="w-32 bg-yellow border-8 border-black p-2 rounded-xl"
+											value={firstNote.toString()}
+											onChange={(e) => {
+												setFirstNote(Number(e.target.value));
+											}}
+										>
+											<option value="12">C0</option>
+											<option value="14">D0</option>
+											<option value="16">E0</option>
+											<option value="17">F0</option>
+											<option value="19">G0</option>
+											<option value="21">A0</option>
+											<option value="23">B0</option>
+											<option value="24">C1</option>
+											<option value="26">D1</option>
+											<option value="28">E1</option>
+											<option value="29">F1</option>
+											<option value="31">G1</option>
+											<option value="33">A1</option>
+											<option value="35">B1</option>
+											<option value="36">C2</option>
+											<option value="38">D2</option>
+											<option value="40">E2</option>
+											<option value="41">F2</option>
+											<option value="43">G2</option>
+											<option value="45">A2</option>
+											<option value="47">B2</option>
+											<option value="48">C3</option>
+											<option value="50">D3</option>
+											<option value="52">E3</option>
+											<option value="53">F3</option>
+											<option value="55">G3</option>
+											<option value="57">A3</option>
+											<option value="59">B3</option>
+											<option value="60">C4</option>
+											<option value="62">D4</option>
+											<option value="64">E4</option>
+											<option value="65">F4</option>
+											<option value="67">G4</option>
+											<option value="69">A4</option>
+											<option value="71">B4</option>
+											<option value="72">C5</option>
+											<option value="74">D5</option>
+											<option value="76">E5</option>
+											<option value="77">F5</option>
+											<option value="79">G5</option>
+											<option value="81">A5</option>
+											<option value="83">B5</option>
+											<option value="84">C6</option>
+											<option value="86">D6</option>
+											<option value="88">E6</option>
+											<option value="89">F6</option>
+											<option value="91">G6</option>
+											<option value="93">A6</option>
+											<option value="95">B6</option>
+											<option value="96">C7</option>
+											<option value="98">D7</option>
+											<option value="100">E7</option>
+											<option value="101">F7</option>
+											<option value="103">G7</option>
+											<option value="105">A7</option>
+											<option value="107">B7</option>
+											<option value="108">C8</option>
+											<option value="110">D8</option>
+											<option value="112">E8</option>
+											<option value="113">F8</option>
+											<option value="115">G8</option>
+											<option value="117">A8</option>
+											<option value="119">B8</option>
+											<option value="120">C9</option>
+											<option value="122">D9</option>
+											<option value="124">E9</option>
+											<option value="125">F9</option>
+											<option value="127">G9</option>
+										</select>
 									</div>
 
-									<div className="flex space-x-2">
-										<button className="bg-yellow border-8 border-black p-2 rounded-xl">
-											Start Recording
-										</button>
+									<div className="flex items-center space-x-2">
+										<label className="text-xl" htmlFor="firstNote">
+											Last Note:
+										</label>
+										<select
+											className="w-32 bg-yellow border-8 border-black p-2 rounded-xl"
+											value={lastNote.toString()}
+											onChange={(e) => {
+												setLastNote(Number(e.target.value));
+											}}
+										>
+											<option value="12">C0</option>
+											<option value="14">D0</option>
+											<option value="16">E0</option>
+											<option value="17">F0</option>
+											<option value="19">G0</option>
+											<option value="21">A0</option>
+											<option value="23">B0</option>
+											<option value="24">C1</option>
+											<option value="26">D1</option>
+											<option value="28">E1</option>
+											<option value="29">F1</option>
+											<option value="31">G1</option>
+											<option value="33">A1</option>
+											<option value="35">B1</option>
+											<option value="36">C2</option>
+											<option value="38">D2</option>
+											<option value="40">E2</option>
+											<option value="41">F2</option>
+											<option value="43">G2</option>
+											<option value="45">A2</option>
+											<option value="47">B2</option>
+											<option value="48">C3</option>
+											<option value="50">D3</option>
+											<option value="52">E3</option>
+											<option value="53">F3</option>
+											<option value="55">G3</option>
+											<option value="57">A3</option>
+											<option value="59">B3</option>
+											<option value="60">C4</option>
+											<option value="62">D4</option>
+											<option value="64">E4</option>
+											<option value="65">F4</option>
+											<option value="67">G4</option>
+											<option value="69">A4</option>
+											<option value="71">B4</option>
+											<option value="72">C5</option>
+											<option value="74">D5</option>
+											<option value="76">E5</option>
+											<option value="77">F5</option>
+											<option value="79">G5</option>
+											<option value="81">A5</option>
+											<option value="83">B5</option>
+											<option value="84">C6</option>
+											<option value="86">D6</option>
+											<option value="88">E6</option>
+											<option value="89">F6</option>
+											<option value="91">G6</option>
+											<option value="93">A6</option>
+											<option value="95">B6</option>
+											<option value="96">C7</option>
+											<option value="98">D7</option>
+											<option value="100">E7</option>
+											<option value="101">F7</option>
+											<option value="103">G7</option>
+											<option value="105">A7</option>
+											<option value="107">B7</option>
+											<option value="108">C8</option>
+											<option value="110">D8</option>
+											<option value="112">E8</option>
+											<option value="113">F8</option>
+											<option value="115">G8</option>
+											<option value="117">A8</option>
+											<option value="119">B8</option>
+											<option value="120">C9</option>
+											<option value="122">D9</option>
+											<option value="124">E9</option>
+											<option value="125">F9</option>
+											<option value="127">G9</option>
+										</select>
 									</div>
 								</div>
 
@@ -241,11 +239,37 @@ const Piano = () => {
 												note: MidiNumbers.getAttributes(midiNumber).note,
 											});
 										}}
+										onPlayNoteInput={(
+											midiNumber: number,
+											{ prevActiveNotes }: any
+										) => {
+											console.log(prevActiveNotes);
+											piano.start({
+												note: MidiNumbers.getAttributes(midiNumber).note,
+											});
+										}}
 										stopNote={(midiNumber: number) => {
 											piano.stop(midiNumber);
 										}}
 										keyboardShortcuts={keyboardShortcuts}
 									/>
+								</div>
+
+								<div className="w-full flex items-end space-x-4">
+									<button
+										onClick={() => setRecording(!recording)}
+										className="bg-yellow border-8 border-black p-2 rounded-xl"
+									>
+										{recording ? "Stop" : "Start"} Recording
+									</button>
+								</div>
+
+								<div className="w-full overflow-x-scroll">
+									{recording
+										? "It's recording! Click some keys to record a sound."
+										: sequence.length == 0
+										? `Click "Start Recording" to record a sound.`
+										: ""}
 								</div>
 							</div>
 						) : (
